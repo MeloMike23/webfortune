de5c1bef058d6c094d02c57ad9a2ebbb5d5c9dba
MeloMike23 <michael.melohn23@gmail.com>
2021-05-06 01:17:00 -0500

    Updated code

--- appserver.py
+++ appserver.py
@@ -1,5 +1,6 @@
-from flask import Flask
-import os
+
+from flask import Flask, redirect, url_for
+import subprocess
 
 app = Flask(__name__)
 
@@ -9,13 +10,21 @@ def index():
 
 @app.route('/fortune/')
 def fortune():
-	os.system('fortune')
+	out = subprocess.run(['fortune'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	res = out.stdout.decode("utf-8")
+	return '<pre>'+res+'</pre>'
 
 @approute('/cowsay/<message>/')
 def cowsay(message):
-	os.system('cowsay' + message)
+	moo = 'cowsay' + message
+	out = subprocess.run([moo], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	res = out.stdout.decode("utf-8")
+	return '<pre>'+res+'</pre>'
 	
 
 @app.route('/cowfortune/')
 def cowfortune():
-	os.system('fortune | cowsay')
+	outf = subprocess.run(['fortune'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	outc = subprocess.run(['cowsay'], input=outf.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	res = cow.stdout.decode("utf-8")
+	return '<pre>'+res+'</pre>'
